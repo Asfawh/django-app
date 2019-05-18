6cfdfeeaedea1b35669df20fd8c89fd1e7038ca9
HABTAMU ASFAW <asfawh@uw.edu>
2019-05-17 23:48:15 -0700

    add list view with template

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+from blogging.models import Post
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    # template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    # body = template.render(context)
+    return render(request, 'blogging/list.html', context)
+
+
+# def list_view(request):
+#     published = Post.objects.exclude(published_date_exact=None)
+#     posts = published.order_by('-published_date')
+#     template = loader.get_template('blogging/list.html')
+#     context = {'posts': posts}
+#     body = template.render(context)
+#     return HttpResponse(body, content_type="text/html")
+
+
+# def stub_view(request, *args, **kwargs):
+#     body = "Stub View\n\n"
+#     if args:
+#         body += "Args:\n"
+#         body += "\n".join(["\t%s" % a for a in args])
+#     if kwargs:
+#         body += "Kwargs:\n"
+#         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+#     return HttpResponse(body, content_type="text/plain")
 
 
