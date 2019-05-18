2dd3d3fdb21031c5f0d24e7ab92301386ccbec56
HABTAMU ASFAW <asfawh@uw.edu>
2019-05-17 21:29:38 -0700

    add categorizing posts and improve categories model

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from blogging.models import Post
+from blogging.models import Post, Category
 
 # add a new admin registration
 admin.site.register(Post)
+admin.site.register(Category)
